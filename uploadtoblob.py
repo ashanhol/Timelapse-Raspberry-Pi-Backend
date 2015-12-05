7fc72eaf99e668a85f8e90dabf3b94f56d59e4b9
Adina Shanholtz <adinas@microsoft.com>
2015-12-04 23:45:43 -0500

    took out account key

--- uploadtoblob.py
+++ uploadtoblob.py
@@ -4,8 +4,8 @@ import uuid
 
 print("debug")
 
-blob_service = BlobService(account_name='frickimages', account_key='xmQhvZyBj5mswE0Pk5Bk2VjHli1R3FXWCWqA4ALvbheCAXtoVEu+l2RC3JLeCSREYX17N7SsUUJtkM8kSx4erg==')
 
+blob_service = BlobService(account_name='{PUT YOUR ACCOUNT NAME HERE}', account_key='{PUT YOUR ACCOUNT KEY HERE}')
 
 blob_service.create_container('mycontainer', x_ms_blob_public_access='container')
 while True: 
